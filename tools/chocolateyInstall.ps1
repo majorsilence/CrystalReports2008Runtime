a78f88cd116063b7f3b7439ea42db868567749fc
Peter Gill <peter.gill@townsuite.com>
2016-12-08 14:51:08 -0330

    Update install script

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -8,30 +8,18 @@ $packageName = $package;
 $silentArgs = '/quiet';
 $url = 'https://smpdl.sap-ag.de/~sapidp/012002523100004495572015D/CRREDIST07_0-10007442.ZIP';
 
+$chocTempDir = Join-Path $env:TEMP "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+$setupMsi = "CRRuntime_12_7_mlb.msi"
+$zipFile = "cr2008sp7_redist.zip"
 
-try {
+$tempDownloadFolder 
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+      
+$file = Join-Path $tempDir $zipFile
+Get-ChocolateyWebFile $packageName $file $url
+Get-ChocolateyUnzip $file $tempDir "" $packageName
 
+$fileMsi = Join-Path $tempDir $setupMsi 
+Install-ChocolateyInstallPackage -packageName $packageName -fileType 'msi' -silentArgs $silentArgs -file $fileMsi
 
-
-  $chocTempDir = Join-Path $env:TEMP "chocolatey"
-  $tempDir = Join-Path $chocTempDir "$packageName"
-  $setupMsi = "CRRuntime_12_7_mlb.msi"
-  $zipFile = "cr2008sp7_redist.zip"
-  
-  $tempDownloadFolder 
-  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
-        
-  $file = Join-Path $tempDir $zipFile
-  Get-ChocolateyWebFile $packageName $file $url
-  Get-ChocolateyUnzip $file $tempDir "" $packageName
-  
-  $fileMsi = Join-Path $tempDir $setupMsi 
-  Install-ChocolateyInstallPackage -packageName $packageName -fileType 'msi' -silentArgs $silentArgs -file $fileMsi
-
-  
-   # the following is all part of error handling
-  Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
-}
